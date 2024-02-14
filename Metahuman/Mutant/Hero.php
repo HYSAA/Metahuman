6563b0b22bfd57863d0d06f5a84770d516fc3517
Alberto Anunciado Jr <92003785+HYSAA@users.noreply.github.com>
2024-02-14 15:19:02 +0800

    Add files via upload

---
+++ Metahuman/Mutant/Hero.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Metahuman\Mutant;
+
+class Hero
+{
+    public function __construct()
+    {
+        echo "I am a Mutant Hero!\n";
+    }
+}
---
+++ Metahuman/Mutant/Mutant.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Metahuman\Mutant;
+
+class Mutant
+{
+    public function __construct()
+    {
+        echo "I am a Mutant!\n";
+    }
+}
